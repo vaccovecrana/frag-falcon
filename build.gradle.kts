ab6990c298c60adb30dcec8b372ac5e85ac38090
Jesus Zazueta <jjzazuet@gmail.com>
2024-08-18 16:33:43 -0400

    - Native build

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ subprojects {
   apply(plugin = "io.vacco.oss.gitflow")
 
   group = "io.vacco.ff"
-  version = "0.5.0"
+  version = "0.5.3"
 
   configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
     addClasspathHell()
